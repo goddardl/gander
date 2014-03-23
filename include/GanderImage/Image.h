2baa61f10008d11fcf6214f57af1056882142fb7
goddardl <general@lukegoddard.info>
2014-03-22 18:33:05 -0700

    Continued to flesh out the Image class.

--- include/GanderImage/Image.h
+++ include/GanderImage/Image.h
@@ -39,6 +39,7 @@
 #include "Gander/Common.h"
 
 #include "GanderImage/Pixel.h"
+#include "GanderImage/Row.h"
 
 namespace Gander
 {
@@ -55,10 +56,81 @@ class Image
 		typedef Image< Layout > Type;
 		typedef typename Gander::Image::Pixel< Layout > Pixel;
 		typedef typename Gander::Image::PixelAccessor< Layout > PixelAccessor;
+		typedef typename Gander::Image::Row< Layout > Row;
 		
 		template< EnumType Index > struct LayoutTraits : public Layout::template LayoutTraits< Index > {};
 		template< ChannelDefault C, bool DisableStaticAsserts = false > struct ChannelTraits : public Layout::template ChannelTraits< C, DisableStaticAsserts > {};
 		template< EnumType Index > struct ChannelTraitsAtIndex : public Layout::template ChannelTraitsAtIndex< Index > {};
+
+		/// Constructs a new image of a specified width and height.
+		inline Image( int32u width, int32 height ) : m_width( width ), m_height( height ) {}
+
+		inline bool isValid() const
+		{
+			return m_pixelAccessor.requiredChannels() == m_availableChannels();
+		}
+
+		inline unsigned int numberOfChannels() const { return m_pixelAccessor.numberOfChannels(); }
+		inline ChannelSet channels() const { return m_pixelAccessor.channels(); }
+		inline bool isDynamic() const { return m_pixelAccessor.isDynamic(); }
+		inline unsigned int numberOfChannelPointers() const { return m_pixelAccessor.numberOfChannelPointers(); };
+		inline ChannelSet requiredChannels() const { return m_pixelAccessor.requiredChannels(); };
+
+		/// Replaces the contents of a channel with a new one. If the channel does not exist then it is created.	
+		/// @param channel The channel to create or assign to.
+		/// @param buf A pointer to the channel data.
+		/// @param stride The distance in bytes between the first element in a row and the first in the next.
+		/// If the stride is negative then the image is flipped vertically.
+		void addChannel( Channel channel, void *buf, size_t stride )
+		{
+			unsigned int index = 0;	
+			
+			if( stride < 0 ) 
+			{ 
+				buf = buf - ( static_cast<size_t>( m_height ) - 1 ) * stride;
+			}
+
+			if( requiredChannels().contains( channel ) )
+			{
+				// Find out if we are replacing a channel or inserting a new one.
+				if( m_availableChannels.contains( channel ) ) // Exists.
+				{
+					unsigned int index = m_availableChannels.index( channel );
+					m_strides[index] = stride;
+				}
+				else // Does not exist.
+				{
+					m_availableChannels += channel;
+					unsigned int index = m_availableChannels.index( channel );
+					m_strides.insert( m_strides.begin() + index, stride );
+				}
+			}
+			else if( isDynamic() )
+			{
+//				here we should add the new channel.
+//				m_pixelAccessor.addChannel();
+//				index = m_channels.index( channel );
+			}
+			else
+			{
+				GANDER_ASSERT( false, "This image does not have a dynamic layout and can't have channels added." );
+			}
+		}
+
+	private :
+
+		/// Holds a pointer to the start of each channel in the image.	
+		PixelAccessor m_pixelAccessor;
+
+		/// Holds a list of the stride values for each channel. The values are ordered by channel index.
+		std::vector< size_t > m_strides;
+
+		/// Holds a set of all of the channels that have been set. This value is used
+		/// to verify if the image is valid or not.
+		ChannelSet m_availableChannels;
+
+		unsigned int m_width;
+		unsigned int m_height;
 };
 
 }; // namespace Image
