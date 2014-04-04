a997dd1e6f3fd2ec49c31be100b15ab604797380
goddardl <lukeg@image-engine.com>
2014-04-04 09:36:05 -0700

    Added a missing include.

--- include/Gander/Assert.h
+++ include/Gander/Assert.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2013, Luke Goddard. All rights reserved.
+//  Copyright (c) 2013-2014, Luke Goddard. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are
@@ -34,6 +34,7 @@
 #ifndef __GANDER_ASSERT_H__
 #define __GANDER_ASSERT_H__
 
+#include <stdexcept>
 #include "Gander/Common.h"
 
 namespace Gander
